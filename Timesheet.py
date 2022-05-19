0b5e2ed25840686963b74a223633f084b5c102c2
Snowballx7370 <monicabcho@gmail.com>
2022-05-20 05:19:07 +0800

    Added Chromedriver.exe Features: 3. Change Shift Schedule Filing

--- Timesheet.py
+++ Timesheet.py
@@ -10,10 +10,15 @@ import time
 import datetime
 #Test
 ####USER INPUT####
+#login
 clientcode = 'YONDU'
 employeecode = '22-02762'
 password = 'Aa051498!'
+#OB filing
 filingremarks = 'No Time In/Time Out'
+#Change shift schedule filing
+changeshiftremarks = 'Change Shift'
+daytype = 'Regular Day'
 excel_files = 'D:\RPA\Yondu Timesheet_Novemberr 1-15 2021_Monica Cho.xlsx'
 
 ####FROM EXCEL####
@@ -22,7 +27,14 @@ timeoutvalues = []
 datevalues = []
 timeinvaluefinal = []
 timeoutvaluefinal = []
-
+changetimein = []
+changetimeout = []
+changetimeinampm = []
+changetimeoutampm = []
+changetimeinfinal = []
+changetimeoutfinal = []
+changetimeinampmfinal = []
+changetimeoutampmfinal = []
 ####EXCEL####
 #LOAD WORKSHEET
 sheet = load_workbook(excel_files)
@@ -57,9 +69,20 @@ for i in range(iteration):
     timeinvaluefinal.append(timeinformat)
     timeoutformat = timeoutfinal[i].strftime("%I:%M %p")
     timeoutvaluefinal.append(timeoutformat)
-#print("TIME IN: " + str(timeinvaluefinal))
-#print("TIME OUT: " + str(timeoutvaluefinal))
-#print("DATE: " + str(datefinal))
+
+    changetimein = timeinfinal[i].strftime("%I")
+    changetimeinfinal.append(int(changetimein))
+    changetimeout = timeoutfinal[i].strftime("%I")
+    changetimeoutfinal.append(int(changetimeout))
+    changetimeinampm = timeinfinal[i].strftime("%p")
+    changetimeinampmfinal.append(changetimeinampm)
+    changetimeoutampm = timeoutfinal[i].strftime("%p")
+    changetimeoutampmfinal.append(changetimeoutampm)
+print("TIME IN: " + str(timeinvaluefinal))
+print("TIME OUT: " + str(timeoutvaluefinal))
+print("CHANGE TIME IN: " + str(changetimeinfinal) + str(changetimeinampmfinal))
+print("CHANGE TIME OUT: " + str(changetimeoutfinal) + str(changetimeoutampmfinal))
+print("DATE: " + str(datefinal))
 
 ####WEB AUTOMATION####
 url = 'https://ess.payroll.ph/app'
@@ -70,6 +93,7 @@ driver.find_element(By.ID, "loginClientCode").send_keys(clientcode)
 driver.find_element(By.ID, "loginEmployeeCode").send_keys(employeecode)
 driver.find_element(By.ID, "loginPassword").send_keys(password)
 driver.find_element(By.ID, "loginForm").click()
+print("Login DONE")
 #Click OB FILING
 driver.implicitly_wait(15)
 driver.find_element(By.XPATH, '//*[@class="x-portlet-button-img"][@src="/images/flat-icons/clock2.png"]').click()
@@ -79,7 +103,7 @@ for i in range(iteration):
     dateyear = datefinal[i].strftime("%Y")
     dateday = datefinal[i].day
     #Click NEW
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.XPATH, '//*[@class=" x-btn-text icon-new"][text()="New"]').click()
     #Client
     driver.find_element(By.XPATH, '//*[@src="/images/default/s.gif"][@class="x-form-trigger x-form-arrow-trigger"]').click()
@@ -106,4 +130,39 @@ for i in range(iteration):
     driver.find_element(By.XPATH, '//*[@id="maintenance-save"]/tbody/tr[2]/td[2]/em/button[text()="Save"]').click()
     driver.find_element(By.XPATH, '//body').click()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="x-btn   x-btn-noicon "]/tbody/tr[2]/td[2]/em/button[text()="Yes"]'))).click()
-#driver.quit()
+print("OB Filing DONE")
+#CLICK Change Shift Schedule Filing
+time.sleep(5)
+driver.find_element(By.XPATH, '//*/li[@id="tabpanel__tab-obfiling"]/a[@class="x-tab-strip-close"]').click()
+driver.find_element(By.XPATH, '//*/div[@id="portal-second-column"]/div/div[2]/div[1]/div/div/div/div[4]/center/img[@src="/images/flat-icons/week.png"]').click()
+for i in range(iteration):
+    ###VARIABLES####
+    datemonth = datefinal[i].strftime("%b")
+    dateyear = datefinal[i].strftime("%Y")
+    dateday = datefinal[i].day
+    #Click NEW
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@class=" x-btn-text icon-new"][text()="New"]').click()
+    #Date
+    driver.find_element(By.XPATH, '//*/div/div/img[@src="/images/default/s.gif"][@class="x-form-trigger x-form-date-trigger"]').click()
+    driver.find_element(By.XPATH, '//*/em[@class=" x-btn-arrow"]').click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="x-date-mp"]/table/tbody/tr/td/a[text() = "' + datemonth + '"]'))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="x-date-mp"]/table/tbody/tr/td/a[text() = "' + dateyear + '"]'))).click()
+    driver.find_element(By.CLASS_NAME, "x-date-mp-ok").click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="x-date-picker x-unselectable"]/table/tbody/tr/td/table/tbody/tr/td/a/em/span[text() = "' + str(dateday) + '"]'))).click()
+    time.sleep(1)
+    #New Schedule
+    driver.find_element(By.XPATH, '//*/form/div[2]/div[1]/div/input[2]').send_keys(str(changetimeinfinal[i]) + changetimeinampmfinal[i] + "-" + str(changetimeoutfinal[i]) + changetimeoutampmfinal[i])
+    time.sleep(1)
+    #Day Type
+    driver.find_element(By.XPATH, '//*/form/div[3]/div[1]/div/input[2]').send_keys(daytype)
+    time.sleep(1)
+    #Input Remarks
+    driver.find_element(By.XPATH, '//*[@name="Remarks"]').click()
+    driver.find_element(By.XPATH, '//*[@name="Remarks"]').send_keys(changeshiftremarks)
+    time.sleep(1)
+    #Save
+    driver.find_element(By.XPATH, '//*[@id="maintenance-save"]/tbody/tr[2]/td[2]/em/button[text()="Save"]').click()
+    driver.find_element(By.XPATH, '//body').click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="x-btn   x-btn-noicon "]/tbody/tr[2]/td[2]/em/button[text()="Yes"]'))).click()
+print("Change Shift DONE")
